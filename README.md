ec9d56d5e439154a78ae00174670a3c3e7301968
gabrielgio <gabrielg.desouza@gmail.com>
2016-10-12 22:06:46 -0300

    Adds base project and few of the main features.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Log
+
+A simpler way to write logs using Markdown (I don't know yet what exactly will be logged, but I'll figure it out soon).
+
+## A warning
+This is work in progress and that is my very second time ([first here](https://github.com/gabrielgio/dota-responses)) messing with python, so there is some ugly code here and there.
---
+++ log/views.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from models import LogItem
+import markdown2
+
+class LogItemViewModel:
+    
+    def __init__(self, content, title):
+        self.title = title
+        self.content = content
+        
+
+def index(request):
+    log_item = LogItem.objects.get(id=3)
+    html = markdown2.markdown(log_item.md.read())
+    log_item_view = LogItemViewModel(html, log_item.title)
+    return render(request, 'index.html', {'i': log_item_view})
