dcd47612fb7f2fe4b7441e747c4fd32a0c4cb099
gabrielgio <gabrielg.desouza@gmail.com>
2016-11-04 20:51:38 -0200

    Fixes slash rout error

--- log/urls.py
+++ log/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import logout_then_login as django_logout
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add$', views.create, name='add'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^signin/$', django_login, {'template_name': "signin.html"}, name='signin'),
-    url(r'^signout/$', django_logout, {'login_url': '/signin/'}, name='signout')
+    url(r'^signup/?$', views.signup, name='signup'),
+    url(r'^signin/?$', django_login, {'template_name': "signin.html"}, name='signin'),
+    url(r'^signout/?$', django_logout, {'login_url': '/signin/'}, name='signout')
 ]
